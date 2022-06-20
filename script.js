8895bea1c51c707edc941f15f22ca92cfe386c28
Ilya <i.nemtsov@white-soft.ru>
2022-06-20 17:10:22 +1000

    Refactor

--- script.js
+++ script.js
@@ -1,85 +1,79 @@
-import drivers from './motorists.json' assert { type: 'json' };
+import drivers from './motorists.json' assert { type: 'json' } ;
 
-function MakeTr (tr, text, id=false) {
-    let td = document.createElement("td");
+function makeTr (tr, text, id) {
+    const td = document.createElement("td");
     td.innerText = text;
-    td.id = id;
+    if (id) td.id = id;
     tr.append(td);
 }
 
-function onClickEvent (i) {
+function makeDivColor (driverCarColor) {
+    const td = document.getElementById("color");
+    const divColor = document.createElement("div");
+    divColor.id = "divColor";
+    divColor.style.backgroundColor = driverCarColor;
+    td.append(divColor);
+}
+
+function makeInputCheckbox (driverCarIsConvertible) {
+    const td = document.getElementById("isConvertible");
+    const inputCheckbox = document.createElement("input");
+    inputCheckbox.type = "checkbox";
+    inputCheckbox.id = "inputCheckbox";
+    inputCheckbox.checked = driverCarIsConvertible;
+    inputCheckbox.setAttribute("readonly", "read");
+    inputCheckbox.onkeydown = () => false;
+    td.append(inputCheckbox);
+}
+
+function onClickEvent (index) {
+    document.getElementById('block-full').style.display = "block";
+    const driver = drivers[index];
     if (!document.getElementById("trFull")) {
         const tbodyFull = document.getElementById("tbodyFull");
-        let trFull      = document.createElement("tr");
-        trFull.id       = "trFull";
+        const trFull = document.createElement("tr");
+        trFull.id = "trFull";
         tbodyFull.append(trFull);
-        trFull = document.getElementById("trFull");
-        MakeTr(trFull, drivers[i].person.lastname, "lastname");
-        MakeTr(trFull, drivers[i].person.firstname, "firstname")
-        MakeTr(trFull, drivers[i].car.manufacturer, "manufacturer");
-        MakeTr(trFull, drivers[i].car.model, "model");
-        MakeTr(trFull, drivers[i].car.year, "year");
-        MakeTr(trFull, drivers[i].car.type, "type");
-        MakeTr(trFull, drivers[i].car.color, "color");
-        let td = document.getElementById("color");
-        let divColor = document.createElement("div");
-        divColor.id = "divColor";
-        divColor.style.backgroundColor = drivers[i].car.color;
-        divColor.style.width = "15px";
-        divColor.style.height = "15px";
-        td.append(divColor);
-        MakeTr(trFull, '', "isConvertible");
-        td = document.getElementById("isConvertible");
-        let inputCkeckbox = document.createElement("input");
-        inputCkeckbox.type = "checkbox";
-        inputCkeckbox.id = "inputCkeckbox";
-        inputCkeckbox.checked = drivers[i].car.isConvertible;
-        td.append(inputCkeckbox);
-        MakeTr(trFull, drivers[i].car.vin, "vin");
+        makeTr(trFull, driver.person.lastname, "lastname");
+        makeTr(trFull, driver.person.firstname, "firstname")
+        makeTr(trFull, driver.car.manufacturer, "manufacturer");
+        makeTr(trFull, driver.car.model, "model");
+        makeTr(trFull, driver.car.year, "year");
+        makeTr(trFull, driver.car.type, "type");
+        makeTr(trFull, driver.car.color, "color");
+        makeDivColor(driver.car.color);
+        makeTr(trFull, '', "isConvertible");
+        makeInputCheckbox(driver.car.isConvertible);
+        makeTr(trFull, driver.car.vin, "vin");
     } else  {
-        let td = document.getElementById("lastname");
-        td.innerText = drivers[i].person.lastname;
-        td = document.getElementById("firstname");
-        td.innerText = drivers[i].person.firstname;
-        td = document.getElementById("manufacturer");
-        td.innerText = drivers[i].car.manufacturer;
-        td = document.getElementById("model");
-        td.innerText = drivers[i].car.model;
-        td = document.getElementById("year");
-        td.innerText = drivers[i].car.year;
-        td = document.getElementById("type");
-        td.innerText = drivers[i].car.type;
-        td = document.getElementById("color");
-        td.innerText = drivers[i].car.color;
-        let divColor = document.createElement("div");
-        divColor.id = "divColor";
-        divColor.style.backgroundColor = drivers[i].car.color;
-        divColor.style.width = "15px";
-        divColor.style.height = "15px";
-        td.append(divColor);
-        let inputCkeckbox = document.getElementById("inputCkeckbox");
-        inputCkeckbox.checked = drivers[i].car.isConvertible;
-        td = document.getElementById("vin");
-        td.innerText = drivers[i].car.vin;
+        document.getElementById("lastname").innerText = driver.person.lastname;
+        document.getElementById("firstname").innerText = driver.person.firstname;
+        document.getElementById("manufacturer").innerText = driver.car.manufacturer;
+        document.getElementById("model").innerText = driver.car.model;
+        document.getElementById("year").innerText = driver.car.year;
+        document.getElementById("type").innerText = driver.car.type;
+        document.getElementById("color").innerText = driver.car.color;
+        makeDivColor(driver.car.color);
+        document.getElementById("inputCheckbox").checked = driver.car.isConvertible;
+        document.getElementById("vin").innerText = driver.car.vin;
     }
 }
 
-function addRow (drivers) {
+function addRows (drivers) {
     const tbodyShort = document.getElementById('tbodyShort');
-    drivers.forEach(function(item, i, drivers) {
-        let tr = document.createElement('tr');
-        tr.id = i;
-        tr.onclick = function() { onClickEvent (i)};
+    drivers.forEach((item, index) => {
+        const tr = document.createElement('tr');
+        tr.id = index;
+        tr.onclick = () => {onClickEvent (index)};
         tbodyShort.append(tr);
-        tr = document.getElementById(i);
-        MakeTr(tr, drivers[i].person.lastname);
-        MakeTr(tr, drivers[i].person.firstname)
-        MakeTr(tr, drivers[i].car.manufacturer);
-        MakeTr(tr, drivers[i].car.model);
-        MakeTr(tr, drivers[i].car.year);
+        makeTr(tr, item.person.lastname);
+        makeTr(tr, item.person.firstname)
+        makeTr(tr, item.car.manufacturer);
+        makeTr(tr, item.car.model);
+        makeTr(tr, item.car.year);
     });
 }
 
 window.onload = function() {
-    addRow(drivers)
+    addRows(drivers);
 };
